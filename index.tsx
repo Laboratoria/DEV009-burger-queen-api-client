a6123fe9384e5b401e5abc03e87e23b50c0bf7a4
Your Name <apmontillal@gmail.com>
2023-10-09 09:30:16 -0500

    guia y login

---
+++ index.tsx
@@ -0,0 +1,17 @@
+//GUIA!
+
+import Home from "../domains/waiter/home";
+import WaiterLogin from "../domains/waiter/login";
+// import WaiterMenu from "../domains/waiter/menu";
+import { BrowserRouter, Routes as ReactRouter, Route } from "react-router-dom";
+
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <ReactRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="/waiterlogin" element={<WaiterLogin />} />
+      </ReactRouter>
+    </BrowserRouter>
+  );
+}
