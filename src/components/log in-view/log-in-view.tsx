a57a4907d07f98d90d5147703906fb2f59116d41
Your Name <apmontillal@gmail.com>
2023-10-10 09:33:50 -0500

    routeo

--- src/components/log in-view/log-in-view.tsx
+++ src/components/log in-view/log-in-view.tsx
@@ -1,42 +1,54 @@
-    import { useState } from 'react'
-    
+import { useState } from 'react';
 
-const formLogIn = ({setEmail,setPassword},{}) =>{
-    
-     
-      const [email, setEmail] = useState('');
-      const [password, setPassword] = useState('');
+const FormLogIn = ({ setEmail, setPassword }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-      //Faltan las funciones 
-      
-      };
-    
-      return (
-       
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label>Email:</label>
-                <input
-                  type="email"
-                  value={email}
-                  onChange={handleEmailChange}
-                  required
-                />
-              </div>
-              <div>
-                <label>Contraseña:</label>
-                <input
-                  type="password"
-                  value={password}
-                  onChange={handlePasswordChange}
-                  required
-                />
-              </div>
-              <button type="submit">Iniciar Sesión</button>
-            </form>
-          );
-          
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-export default formLogIn;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Aquí deberías hacer la autenticación utilizando las credenciales
+    // y posiblemente la API MOCK
+
+    // Ejemplo:
+    if (email === 'usuario@dominio.com' && password === 'contraseña') {
+      console.log('Usuario autenticado');
+    } else {
+      console.log('Credenciales incorrectas');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div>
+        <label>Contraseña:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+      </div>
+      <button type="submit">Iniciar Sesión</button>
+    </form>
+  );
+};
+
+export default FormLogIn;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+//GUIA!
+
+import Home from "../domains/waiter/home";
+import WaiterLogin from "../domains/waiter/login";
+// import WaiterMenu from "../domains/waiter/menu";
+import { BrowserRouter, Routes as ReactRouter, Route } from "react-router-dom";
+
+export function Routes() {
+  return (
+    <BrowserRouter>
+      <ReactRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="/waiterlogin" element={<WaiterLogin />} />
+      </ReactRouter>
+    </BrowserRouter>
+  );
+}
