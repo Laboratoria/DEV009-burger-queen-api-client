17e722b6b8dd76c6ef13a4888f2c6a9017557e1a
Andrea <andreacabrera37@gmail.com>
2023-10-11 11:49:00 -0600

    agregando botones de menu y orden lista y uniendo con waiterview

--- src/components/waiter-view/waiter-view.tsx
+++ src/components/waiter-view/waiter-view.tsx
@@ -1,11 +1,17 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import useHistory  from 'react-router-dom';
 import OrderForm from "../order-form/order-form";
 import ProductList from "../product-list/product-list";
+import UserProfile from "../user-profile/userprofile";
+import MainMenuButtons from "../main-menu.buttons/main-menu-buttons";
 
 const  WaiterView = () => {
     const [client, setClient] = useState("Kvn");
     const [products, setProducts] = useState([]);
+    const history = useHistory();
+    const [,setAuthenticated] = useState(false);
+
 
     useEffect(() =>{
         fetch("http://localhost:8080/products")
@@ -13,14 +19,33 @@ const  WaiterView = () => {
                 setProducts(response.products);
             })
     }, [])
-
-    return (<>
+    const handleBackClick = () => {
+        history.goBack();
+      };
+    const handleLogoutClick = () => {
+        setAuthenticated(false);
+        history.push("/login");
+    };
     
-        <div><ProductList editable={false} products={products} /></div>
-        <div>Creating order: 
-            <OrderForm client={client} />
-        </div>
-    </>);
+    return (
+    <>
+    <UserProfile 
+          profileType="waiter"
+          waiterName="waiter1"
+          onBackClick={handleBackClick}
+          onLogoutClick={handleLogoutClick} administratorName={""} cookName={""}    />
+    <div>
+    <MainMenuButtons /> 
+    </div>
+    <div>
+      <ProductList editable={false} products={products} />
+    </div>
+    <div>
+      Creating order:
+      <OrderForm client={client} />
+    </div>
+  </>
+);
 };
 
 export default WaiterView;
