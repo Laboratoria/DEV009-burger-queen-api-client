53dc69f5feec6b3c54e333b07190c977be52d11f
Andrea <andreacabrera37@gmail.com>
2023-10-11 16:01:59 -0600

    waiter view cambio en ruta de react

--- src/components/waiter-view/waiter-view.tsx
+++ src/components/waiter-view/waiter-view.tsx
@@ -1,51 +1,51 @@
 import { useEffect } from "react";
 import { useState } from "react";
-import useHistory  from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import OrderForm from "../order-form/order-form";
 import ProductList from "../product-list/product-list";
 import UserProfile from "../user-profile/userprofile";
 import MainMenuButtons from "../main-menu.buttons/main-menu-buttons";
 
-const  WaiterView = () => {
-    const [client, setClient] = useState("Kvn");
-    const [products, setProducts] = useState([]);
-    const history = useHistory();
-    const [,setAuthenticated] = useState(false);
+const WaiterView = () => {
+  const [client, setClient] = useState("Kvn");
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+  const [, setAuthenticated] = useState(false);
 
 
-    useEffect(() =>{
-        fetch("http://localhost:8080/products")
-            .then((response) => {
-                setProducts(response.products);
-            })
-    }, [])
-    const handleBackClick = () => {
-        history.goBack();
-      };
-    const handleLogoutClick = () => {
-        setAuthenticated(false);
-        history.push("/login");
-    };
-    
-    return (
+  useEffect(() => {
+    fetch("http://localhost:8080/products")
+      .then((response) => {
+        setProducts(response.products);
+      })
+  }, [])
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+  const handleLogoutClick = () => {
+    setAuthenticated(false);
+    history.push("/login");
+  };
+
+  return (
     <>
-    <UserProfile 
-          profileType="waiter"
-          waiterName="waiter1"
-          onBackClick={handleBackClick}
-          onLogoutClick={handleLogoutClick} administratorName={""} cookName={""}    />
-    <div>
-    <MainMenuButtons /> 
-    </div>
-    <div>
-      <ProductList editable={false} products={products} />
-    </div>
-    <div>
-      Creating order:
-      <OrderForm client={client} />
-    </div>
-  </>
-);
+      <UserProfile
+        profileType="waiter"
+        waiterName="waiter1"
+        onBackClick={handleBackClick}
+        onLogoutClick={handleLogoutClick} administratorName={""} cookName={""} />
+      <div>
+        <MainMenuButtons />
+      </div>
+      <div>
+        <ProductList editable={false} products={products} />
+      </div>
+      <div>
+        Creating order:
+        <OrderForm client={client} />
+      </div>
+    </>
+  );
 };
 
 export default WaiterView;
