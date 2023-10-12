c52941fd17dbce5e8da16bfae54ef6ecd3801fdd
Your Name <apmontillal@gmail.com>
2023-10-12 16:35:33 -0500

    login con role

--- src/components/log in-view/log-in-view.tsx
+++ src/components/log in-view/log-in-view.tsx
@@ -1,47 +1,62 @@
-import { useState } from "react";
+import  { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import '../../index.css';
+import "../../index.css";
 import { login } from "../../services/tokenRepository";
-
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  const handleFormSubmit = async (e: { preventDefault: () => void; }) => { //envia el formulario
-    e.preventDefault(); //detiene el comportamiento predeterminado del evento.
-    try {
-      const data = await login(email, password)
-      console.log('Usuario autenticado como mesero');
-      navigate('/waiter-view'); // Redirige al mesero a la vista del mesero
+  const handleFormSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
 
+    try {
+      const data = await login(email, password);
+      const userRole = localStorage.getItem("userRole");
+
+      console.log(`userRole from localStorage: ${userRole}`);
+
+      switch (userRole) {
+        case 'waiter':
+          console.log('Usuario autenticado como mesero');
+          navigate('/waiter-view');
+          break;
+        case 'admin':
+          console.log('Usuario autenticado como administrador');
+          navigate('/admin-view');
+          break;
+        case 'chef':
+          console.log('Usuario autenticado como chef');
+          navigate('/chef-view');
+          break;
+        default:
+          console.log(`Rol desconocido: ${userRole}`);
+      }
     } catch (error) {
       console.error(error);
-
     }
   };
 
-  
   return (
     <form onSubmit={handleFormSubmit} id="logInBody">
-       <img className="logo" src="../img/f22f65b8-9001-4b5e-8721-bd87b5d636d2.jpg" alt="Logo" />
-      <div id= "cuadrito">
-      <input
-        id="emailbox"
-        type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="ENTER EMAIL"
-      />
-      <input
-        id="passwordbox"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="ENTER PASSWORD"
-      />
-      <button type="submit" id="loginButton">LOGIN</button>
+      <img className="logo" src="../img/f22f65b8-9001-4b5e-8721-bd87b5d636d2.jpg" alt="Logo" />
+      <div id="cuadrito">
+        <input
+          id="emailbox"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="ENTER EMAIL"
+        />
+        <input
+          id="passwordbox"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="ENTER PASSWORD"
+        />
+        <button type="submit" id="loginButton">LOGIN</button>
       </div>
     </form>
   );
--- src/services/tokenRepository.ts
+++ src/services/tokenRepository.ts
@@ -1,28 +1,30 @@
 export const login = async (email: string, password: string) => {
-    
-    const response = await fetch("http://localhost:8080/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email,
-        password,
-      }),
-    });
+  const response = await fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
 
-    if (response.ok) {
-      const data = await response.json();
-      const accessToken = data.accessToken;
-      //donde se esta guardando
-      localStorage.setItem("accessToken", accessToken); 
-      return data; 
-    }
-    else {
-        throw new Error("Ha ocurrido un error en la petición");
-        
-    }
+  if (response.ok) {
+    const data = await response.json();
 
-   
-}
-  
+
+    const userRole = data.user.role; 
+    localStorage.setItem("userRole", userRole);
+
+
+    localStorage.setItem("accessToken", data.accessToken);
+
+    return data;
+  } else {
+    throw new Error("Ha ocurrido un error en la petición");
+  }
+};
+
+
+
